c745cea1468efec6db8f7391f077e7078986686b
vikaschandel <vikassingh438@gmail.com>
2024-09-19 17:18:46 +0530

    first commit export format scddd dd

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -615,6 +615,8 @@ class OAuthController extends Controller
                             $gstins = $this->extractGSTINsFromBuyerSection($buyerSection);
             
                             $fileInfos[] = [
+                                'Invoice no' => $invoiceNo,
+                                'Invoice timestamp' => $file['lastModifiedDateTime'],
                                 'Bill to GST' => $gstins,
                                 'Attachment' => $file['@microsoft.graph.downloadUrl'],
                             ];
